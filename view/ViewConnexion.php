d6054f188464750c7e44f1a6a8b0ce9d7b784c7b
oxylen <dorian_message@hotmail.fr>
2025-04-02 14:06:50 +0200

    add ViewConnexion.php

---
+++ view/ViewConnexion.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="./public/assets/scripts/script.js" defer></script>
+    <link rel="stylesheet" href="./public/assets/style/main.css">
+    <title>Connexion</title>
+</head>
+<body>
+    <h1>Se connecter</h1>
+    <form action="" method="post">
+        <label for="email">Veuillez saisir votre adresse email : </label>
+        <input type="email" name="email">
+        <label for="password">Veuillez saisir votre mot de passe : </label>
+        <input type="passsword" name="password">
+        <input type="submit" name="submit" value="connexion">
+    </form>
+</body>
+</html>
